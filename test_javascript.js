82b10bf65e4ce68045fb170ca8db4317cebbf032
bdlong <buiduc.long@ingenico.com>
2026-06-15 16:30:43 +0700

    first commit, add test_javascript.js

---
+++ test_javascript.js
@@ -0,0 +1,7 @@
+<script>
+    function testingFunction() {
+        console.log("testingFunction start, start alert message")
+        alert("Chào bạn, đây là thông báo cảnh báo!")
+    }
+    testingFunction();
+</script>
